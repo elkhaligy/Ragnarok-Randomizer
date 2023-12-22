e979a1dc116b24b623bf737a646ec0e5b4e16853
Shehab Elkhaligy <shehabelkhaligy100@gmail.com>
2023-12-22 14:04:40 +0200

    A new JS script - Added a DB

--- script.js
+++ script.js
@@ -1,66 +1,14 @@
 function fetchRandomMonster() {
-	const imageContainer = document.getElementById('imageContainer');
-
-	// Clear previous image
+	let imageContainer = document.getElementById('imageContainer');
 	imageContainer.innerHTML = '';
-
-	// Generate a random monster_id (assuming the range is from 1 to 100, adjust as needed)
-	const randomMonsterId = getRandomNumber(1000,2500	);
+	let randomMonsterId = getRandomNumber(1000,2500);
 	console.log(randomMonsterId);
-	// Fetch a random image for the generated monster_id
-		fetch(`https://ragnapi.com/api/v1/old-times/monsters/${randomMonsterId}`)
-		.then(response => {
-			// Check if the response status is OK
-			if (response.ok) {
-				// Parse JSON data from the response
-				return response.json();
-			} else {
-				console.error('Error:', response.statusText);
-				throw new Error('Error fetching monster data');
-			}
-		})
-		.then(data => {
-			console.log(data);
-			const monsterGif = document.createElement('img');
-			monsterGif.src = data.gif;
-			imageContainer.appendChild(monsterGif);
-		})
-		.catch(error => {
-			fetchRandomMonster();
-			console.error('Error fetching random image:', error);
-		});
-}
-function fetchRandomItem() {
-	const imageContainer = document.getElementById('imageContainer');
-
-	// Clear previous image
-	imageContainer.innerHTML = '';
-
-	// Generate a random monster_id (assuming the range is from 1 to 100, adjust as needed)
-	const randomItemId = getRandomNumber(1000,2500);
-	console.log(randomItemId);
-	// Fetch a random image for the generated monster_id
-		fetch(`https://ragnapi.com/api/v1/old-times/items/${randomItemId}`)
-		.then(response => {
-			// Check if the response status is OK
-			if (response.ok) {
-				// Parse JSON data from the response
-				return response.json();
-			} else {
-				console.error('Error:', response.statusText);
-				throw new Error('Error fetching monster data');
-			}
-		})
-		.then(data => {
-			console.log(data);
-			const itemPng = document.createElement('img');
-			itemPng.src = data.img;
-			imageContainer.appendChild(itemPng);
-		})
-		.catch(error => {
-			fetchRandomItem();
-			console.error('Error fetching random image:', error);
-		});
+	let path = 'Database/mobs_gifs/';
+	let tmp = '.gif';
+	let Image_Name = path+randomMonsterId+tmp;
+	let Image_Element = document.createElement('img');
+	Image_Element.src = Image_Name;
+	imageContainer.appendChild(Image_Element);
 }
 function getRandomNumber(min, max) {
     // Ensure that the provided values are numbers
